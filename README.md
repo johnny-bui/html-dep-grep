5d68758c1c15953b8e7d8e12fc0ee1c74e94a752
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2017-02-27 22:49:06 +0100

    work somehow

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ### html-dev-grep
 
+## install
+
+`sudo npm install -g`
+
+
 
--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+#!/usr/bin/env node
+
 const readline = require('readline');
 const fs = require('fs');
 const htmlGrep = require('./html-dev-grep');
-var fileName = "test/test.html";
+
+var program = require('commander');
+
+var fileName = null;
+program
+	.version('0.0.1')
+	.arguments('<file>')
+	//.option('-u, --username <username>', 'The user to authenticate as')
+	//.option('-p, --password <password>', 'The user\'s password')
+	.action(function(file) {
+		console.log('file: %s', file);
+		fileName = file;
+	})
+	.parse(process.argv);
+
+
 var done = function(group){
-	console.log(group);
 	var grepTag = htmlGrep.grepTag(fileName, group);
 	console.log(group);
 };
